017af6f0a41e4e1164d1aa24c00f9efe53d43a62
Iamalishan0 <141748006+Iamalishan0@users.noreply.github.com>
2023-08-08 19:50:31 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <nav style="background-image: url(header-bg.svg); height: 200px;">
+    <label>
+        <img src="22A4FAD0618CD94051548BD71DDB41A2.png" style="height: 75px;">
+    </label>
+    <ul>
+        <li><a href="#"><i class="fa-solid fa-house" style="color: #2ca039;"></i>Home</a></li>
+        <li><a href="#"><i class="fa-solid fa-calculator" style="color: #299e46;"></i>Calculator</a></li>
+        <li><a href="#"><i class="fa-solid fa-hand-holding-dollar" style="color: #44834e;"></i>Services</a></li>
+        <li><a href="#"><i class="fa-solid fa-blog" style="color: #35b14e;"></i>Blog</a></li>
+        <li><a href="#"><i class="fa-regular fa-comment" style="color: #35924c;"></i>Reviews</a></li>
+        <button type="button" class="btn btn-primary">Contact</button>
+</ul>
+</nav>
+<div>
+    <img src="banner.svg" style="height: 300px; margin-left: 10%; float: left;">
+    <button type="button" class="btn btn-success" style="margin-left: 7%;">Loan EMI calculator</button>
+     <div class="loan"><h1 style="font-weight: bold;">A personal loan for <br>multiple purposes!</h1>
+        <h4>Don't let financial constraints hold you back.<br> Take the first step towards your dreams by<br> applying for a personal loan today. Our<br> user-friendly online application process<br> makes it convenient for you to get <br>the funds you need, when you <br> need them.</h4>
+        <br>
+        <button class="button button3" style="background-color: rgb(80, 173, 205);border-radius: 5px;">Learn more</button>
+     </div>
+</div>
+<div id="Services">
+    <h1> Our Services</h1>
+    <i class="fa-solid fa-bars-progress" style="color: #357e47;" id="pro"></i>
+    <h4 class="hhh">Personal Loans: Flexible funding for dreams.<br>
+        Education Loans: Invest in your future.<br>
+        Home Loans: Step into your dream home.<br>
+        Auto Loans: Drive your dream car.<br>
+        Small Business Loans: Boost your business.<br>
+        Emergency Loans: Quick financial help.<br>
+        </h4>
+      
+<div class="container">
+    <div class="homeloan">
+        <img src="001-credit.png" id="loanimage"><br>
+            <button class="button button3" style="margin-left: 90px;margin-top: 20px;cursor: pointer;" >Home Loan</button>
+            <p style="margin-left: 20px;margin-top: 10px;">"Own your dream home with our competitive Home Loans. Flexible terms, low rates, hassle-free process."</p>
+      <div id="loanimage"></div>
+    </div>
+    <div class="carloan">
+        <img src="002-automobile.png" id="carimg">
+        <button class="button button3" style="margin-left: 100px;margin-top: 20px;cursor: pointer;" >Car Loan</button>
+                <p style="margin-left: 20px;margin-top: 10px;" >"Drive your dream car with our affordable Car Loans. Quick approvals, low rates, hassle-free process."</p>
+      <div id="carimg"></div>
+    </div>
+    <div class="perloan">
+        <img src="003-profits.png" id="perimg">
+        <button class="button button3" style="margin-left: 90px;margin-top: 10px;cursor: pointer;" >Personal Loan</button>
+        <p style="margin-left: 20px; margin-top: 10px;">"Realize your dreams with our Personal Loans. Fast approvals, flexible terms, and reliable support."</p>
+      <div id="perimg"></div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="homeloan">
+        <img src="006-credit-card.png" id="loanimage"><br>
+            <button class="button button3" style="margin-left: 60px;margin-top: 20px;cursor: pointer;" >Small business Loan</button>
+            <p style="margin-left: 20px;margin-top: 10px;">"Fuel your small business with our flexible Small Business Loans. Quick approvals, low rates."</p>
+      <div id="loanimage"></div>
+    </div>
+    <div class="carloan">
+        <img src="004-ingots.png" id="carimg">
+        <button class="button button3" style="margin-left: 80px;margin-top: 20px;cursor: pointer;" >Emergency Loans</button>
+                <p style="margin-left: 20px;margin-top: 10px;" >
+                    "Get quick financial support during emergencies with our reliable Emergency Loans. Fast approvals, no delays."</p>
+      <div id="carimg"></div>
+    </div>
+    <div class="perloan">
+        <img src="005-mortarboard.png" id="perimg">
+        <button class="button button3" style="margin-left: 80px;margin-top: 10px;cursor: pointer;" >Education Loans:</button>
+        <p style="margin-left: 20px; margin-top: 10px;">"Realize your dreams with our Personal Loans. Fast approvals, flexible terms, and reliable support.""Invest in your future,"</p>
+      <div id="perimg"></div>
+    </div>
+  </div>
+
+  <div>
+    <h1 style="margin-top: 30px;margin-left: 0px;color: rgb(18, 18, 24);padding: 8px;">
+      Latest Blog
+      </h1>
+      <i class="fa-solid fa-bars-progress" style="color: #357e47; margin-top:0px;margin-left: 8px;"></i>
+      <h4 style="margin-left: 7px;">
+        Unlock possibilities with our diverse loans:<br> Home Loans for your dream abode,<br> Personal Loans for life's needs,<br> and Business Loans for entrepreneurial success.<br> Apply now for financial empowerment!
+      </h4>
+      <img src="blog-1.jpg" style="height: 250px; width: 250px; border-radius: 20px;" >
+       
+      <img src="blog-2.jpg" style="height: 250px; width: 250px; border-radius: 20px; margin-left: 20px;" >
+       
+      <img src="blog-3.jpg" style="height: 250px; width: 250px; border-radius: 20px; margin-left: 20px;" >
+      <br> <br> <br>
+      <h2>"Transforming Aspirations into Reality  <br> with Personal Loans" </h2>
+      <br> 
+      <h4>
+      "A loan with low interest rates is a valuable financial tool that offers affordable borrowing options.<br> With reduced financial strain, individuals can pursue their goals and aspirations confidently.<br> It paves the way for brighter opportunities and a more secure financial future, promoting responsible and prudent financial decisions."
+      </h4>      
+  </div>
+
+      <br> 
+
+      <div style="height: 500px; width: 1000px; background-color: rgb(244, 243, 243);">
+        <h1 style="padding: 40px;text-decoration:underline #2ca039;"> Customer Stories</h1>
+        <div style="height: 220px; width: 470px;background-color:rgb(243, 231, 196); margin-left: 40px;margin-top: 0px;">
+         
+         <h1> <i class="fa-regular fa-comment" style="color: #36633d; margin-left: 400px; margin-top: 0px;"></i></h1>
+         <h4> 
+          "Exceptional loan website! Fast and hassle-free application process, competitive interest rates, and excellent customer support. Secured my loan effortlessly. Highly recommended for anyone seeking financial assistance."</h4>
+         <img src="user-1.png" style="margin-top: 10px;"> <h4 style="display: inline-block;margin-top: 10px;">ALISHAN</h4>
+
+         <button  style="margin-left: 240px;margin-top: 30px;"><<</button>
+        <button >>></button> <img src="review.svg" style="height: 350px; width: 300px;margin-left: 550px;margin-top: -300px;">
+        </div>
+      </div>
+
+      <br>
+      
+    <h1 style="text-decoration: underline #2ca039;">Get In Touch</h1>
+     <h3>"Get In Touch: Connecting You to Your Dreams. <br>Realize Your Aspirations with Every Interaction.<br> Your Pathway to Success and Fulfillment." </h3>
+    <img src="contact.svg" style="height: 350px; width: 450px; margin-top: -300px;">
+    <div style="height: 400px; width: 450px;background-color: rgb(250, 238, 222);display: inline-block;margin-left: 70px;padding: 140px; border-radius: 20px;">
+      <h2 style="position: absolute;margin-top: -110px;color: rgb(23, 80, 103);margin-left: -15px;text-decoration: underline;">Send Message</h2>
+        
+      <div style="margin-top: -30px;">
+          <form action="#" method="post">
+          <input type="text" id="username"  name="user" placeholder="Please enter your Name"  style="width: 300px;height: 40px; margin-left: -60px;border-radius: 10px;background-color: rgb(200, 241, 241); text-align: center;">
+          <br> <br> 
+          <input type="text"  name="email" placeholder="Please enter your Email"  style="width: 300px;height: 40px; margin-left: -60px;border-radius: 10px;background-color: rgb(200, 241, 241); text-align: center;">
+          <br> <br> 
+          <input type="text"  name="message" placeholder="Write your Message" style="width: 300px;height: 78px; margin-left: -60px;border-radius: 10px;background-color: rgb(200, 241, 241); text-align: center;"> 
+          <br> <br>
+          <button name="submit"  id="d1" style="height: 30px; width: 80px;margin-left: 50px;background-color: lightcyan;">Submit</button>
+
+          </form>
+        </div>
+    </div>
+  
+</body>
+<br> <br> <br> <br> <br> <br>
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-white text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div> <br> 
+    <!-- Left -->
+
+    <!-- Right -->
+    <div >
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-github"></i>
+      </a>
+    </div> <br>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3 text-secondary"></i>Demo
+          </h6>
+          <p>
+          "Unlocking Possibilities: Your Journey to Success Begins Here."
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Payday Loans</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Mortgage Loans</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Secured Loanse</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Home Equity Loans</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3 text-secondary"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope me-3 text-secondary"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3 text-secondary"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3 text-secondary"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Demo.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+</html>
+<?php
+    $con = mysqli_connect("localhost","root","","crud");
+    if(isset($_POST['submit']))
+    {
+      $username = $_POST['user'];
+      $email = $_POST['email'];
+      $message = $_POST['message'];
+      $query = "insert into users(`name`,`email`,`message`)values('$username','$email','$message')";
+      $qul = mysqli_query($con,$query);
+    }
+  
+    ?>
+    
